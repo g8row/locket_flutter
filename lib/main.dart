8b0dda73295e2f6f24337bdf2fabf729964495de
g8row <aliogu23@gmail.com>
2022-01-30 22:34:17 +0200

    main camera ui working

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:math';
@@ -27,6 +28,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.purple,
         primaryColor: Colors.purple.shade200,
       ),
+      debugShowCheckedModeBanner: false,
       home: const MyHomePage(title: 'locket_flutter'),
     );
   }
@@ -40,31 +42,144 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+
+
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   int id = 1;
   String ftp = "";
   int partnerID = 0;
-  late CameraController controller;
+  XFile? pictureFile;
 
+  late CameraController? controller;
   @override
   void initState() {
-    super.initState();
+    
     getID();
     getPartnerID();
     getFTP();
-    controller = CameraController(cameras[0], ResolutionPreset.max);
-    controller.initialize().then((_) {
+    controller = CameraController(cameras[1], ResolutionPreset.max);
+    controller?.initialize().then((_) {
       if (!mounted) {
         return;
       }
       setState(() {});
     });
+    WidgetsBinding.instance?.addObserver(this);
+
+    //controller!.dispose();
+  }
+
+@override
+  void dispose() {
+    WidgetsBinding.instance?.removeObserver(this);
+    //controller?.dispose();
+    super.dispose();
+  }
+
+
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    final CameraController? cameraController = controller;
+
+    // App state changed before we got the chance to initialize.
+    if (cameraController == null || !cameraController.value.isInitialized) {
+      return;
+    }
+
+    if (state == AppLifecycleState.inactive) {
+      cameraController.dispose();
+    } else if (state == AppLifecycleState.resumed) {
+      initCam();
+      if (mounted) {
+          setState(() {});
+        }
+    }
+  }
+
+  void initCam() {
+    controller?.dispose();
+
+    controller = CameraController(cameras[1], ResolutionPreset.max);
+
+    // If the controller is updated then update the UI.
+    controller?.addListener(() {
+      if (mounted) setState(() {});
+      if (controller!.value.hasError) {
+        print('Camera error ${controller?.value.errorDescription}');
+      }
+    });
+
+    controller?.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }});
   }
 
   @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: <Widget>[
+          IconButton(onPressed: _openSettings, icon: const Icon(Icons.settings))
+        ],
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 40),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(16.0),
+            child: Align(
+              alignment: Alignment.center, 
+              heightFactor: 9/16, 
+              child: CameraPreview(controller!),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20.0),
+            child: ButtonBar(
+              alignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () async { pictureFile = await controller?.takePicture(); print(pictureFile!.path);}, 
+                  child: const Icon(Icons.camera)
+                ),
+                ElevatedButton(
+                  onPressed: _openGallery,
+                  child: const Icon(Icons.photo))
+              ],
+            ),
+          ),
+        ]
+      )),
+    );
+  }
+
+  void _openGallery(){
+    Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext builder) {  
+      return StatefulBuilder(builder: (BuildContext context, StateSetter setState) {
+        Widget gallery = Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 40),
+          child: Center(
+            //crossAxisAlignment: CrossAxisAlignment.center,
+            //mainAxisAlignment: MainAxisAlignment.center,
+            child: 
+              ClipRRect(
+                borderRadius: BorderRadius.circular(16.0),
+                child: Align(
+                  alignment: Alignment.center, 
+                  heightFactor: 9/16, 
+                  child: pictureFile!=null ? Image.file(File(pictureFile!.path)) : Center(),
+                ),
+          )));
+      return Scaffold(
+          appBar: AppBar(title: const Text("Settings")),
+          body: gallery,
+        );
+      });
+    }));
   }
 
   void getID() async {
@@ -99,10 +214,8 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void _openSettings() {
-    Navigator.of(context)
-        .push(MaterialPageRoute(builder: (BuildContext builder) {
-      return StatefulBuilder(
-          builder: (BuildContext context, StateSetter setState) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext builder) {
+      return StatefulBuilder(builder: (BuildContext context, StateSetter setState) {
         Widget settings = Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
@@ -112,7 +225,10 @@ class _MyHomePageState extends State<MyHomePage> {
               child: TextFormField(
                 initialValue: ftp,
                 onFieldSubmitted: (value) {
-                  setPrefsString('ftp', value);
+                  setState((){
+                    setPrefsString('ftp', value);
+                    getFTP();
+                  });
                 },
                 decoration: const InputDecoration(
                   border: OutlineInputBorder(),
@@ -125,7 +241,10 @@ class _MyHomePageState extends State<MyHomePage> {
               child: TextFormField(
                 initialValue: partnerID.toString(),
                 onFieldSubmitted: (value) {
-                  setPrefsInt('partnerID', int.parse(value));
+                  setState(() {
+                    setPrefsInt('partnerID', int.parse(value));
+                    getPartnerID();
+                  });
                 },
                 decoration: const InputDecoration(
                   border: OutlineInputBorder(),
@@ -156,16 +275,5 @@ class _MyHomePageState extends State<MyHomePage> {
     }));
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: <Widget>[
-          IconButton(onPressed: _openSettings, icon: const Icon(Icons.settings))
-        ],
-        title: Text(widget.title),
-      ),
-      body: Center(child: CameraPreview(controller)),
-    );
-  }
+  
 }
